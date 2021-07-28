8601f0ab4a447e22bd67775946e55dae457906cf
Young wook Jeon <ela87@naver.com>
2021-07-29 03:14:13 +0900

    Batch / Transaction

--- redisson-playground/src/test/java/com/young/redisson/test/Lec12PubSubTest.java
+++ redisson-playground/src/test/java/com/young/redisson/test/Lec12PubSubTest.java
@@ -1,14 +1,16 @@
 package com.young.redisson.test;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RPatternTopicReactive;
 import org.redisson.api.RTopicReactive;
+import org.redisson.api.listener.PatternMessageListener;
 import org.redisson.client.codec.StringCodec;
 
 public class Lec12PubSubTest extends BaseTest {
 
     @Test
     public void subscriber1() {
-        RTopicReactive topic = this.client.getTopic("slack-room", StringCodec.INSTANCE);
+        RTopicReactive topic = this.client.getTopic("slack-room1", StringCodec.INSTANCE);
         topic.getMessages(String.class)
                 .doOnError(System.out::println)
                 .doOnNext(System.out::println)
@@ -18,11 +20,14 @@ public class Lec12PubSubTest extends BaseTest {
 
     @Test
     public void subscriber2() {
-        RTopicReactive topic = this.client.getTopic("slack-room", StringCodec.INSTANCE);
-        topic.getMessages(String.class)
-                .doOnError(System.out::println)
-                .doOnNext(System.out::println)
-                .subscribe();
+        RPatternTopicReactive patternTopic = this.client.getPatternTopic("slack-room", StringCodec.INSTANCE);
+        patternTopic.addListener(String.class, new PatternMessageListener<String>() {
+            @Override
+            public void onMessage(CharSequence pattern, CharSequence topic, String msg) {
+                System.out.println(pattern + " : " + topic + " : " + msg);
+            }
+        }).subscribe();
+
         sleep(600_000);
     }
 }
