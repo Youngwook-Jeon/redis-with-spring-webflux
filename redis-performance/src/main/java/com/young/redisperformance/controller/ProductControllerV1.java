7cc77cd3bc28aae15427c22d69ebd30e90e812fc
Young wook Jeon <ela87@naver.com>
2021-08-07 19:09:57 +0900

    Add an utility class for caching

--- redis-performance/src/main/java/com/young/redisperformance/controller/ProductControllerV1.java
+++ redis-performance/src/main/java/com/young/redisperformance/controller/ProductControllerV1.java
@@ -1,25 +1,25 @@
 package com.young.redisperformance.controller;
 
 import com.young.redisperformance.entity.Product;
-import com.young.redisperformance.service.ProductService;
+import com.young.redisperformance.service.ProductServiceV1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/product")
-public class ProductController {
+public class ProductControllerV1 {
 
     @Autowired
-    private ProductService productService;
+    private ProductServiceV1 productServiceV1;
 
     @GetMapping("/{id}")
     public Mono<Product> getProduct(@PathVariable int id) {
-        return this.productService.getProduct(id);
+        return this.productServiceV1.getProduct(id);
     }
 
     @PutMapping("/{id}")
     public Mono<Product> updateProduct(@PathVariable int id, @RequestBody Mono<Product> productMono) {
-        return this.productService.updateProduct(id, productMono);
+        return this.productServiceV1.updateProduct(id, productMono);
     }
 }
---
+++ redis-performance/src/main/java/com/young/redisperformance/service/ProductServiceV2.java
@@ -0,0 +1,4 @@
+package com.young.redisperformance.service;
+
+public class ProductServiceV2 {
+}
